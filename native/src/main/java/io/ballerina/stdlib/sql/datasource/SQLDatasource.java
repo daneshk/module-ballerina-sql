4210262ce9d7ae0094de7299b66f32812ce0dd41
Danesh Kuruppu <dknkuruppu@gmail.com>
2025-03-17 19:32:55 +0530

    remote string conversion in connection properties

--- native/src/main/java/io/ballerina/stdlib/sql/datasource/SQLDatasource.java
+++ native/src/main/java/io/ballerina/stdlib/sql/datasource/SQLDatasource.java
@@ -397,7 +397,7 @@ public class SQLDatasource {
             if (sqlDatasourceParams.options != null) {
                 BMap<BString, Object> optionMap = (BMap<BString, Object>) sqlDatasourceParams.options;
                 optionMap.entrySet().forEach(entry ->
-                        xaProperties.setProperty(entry.getKey().getValue(), entry.getValue().toString())
+                        xaProperties.putIfAbsent(entry.getKey().getValue(), entry.getValue())
                 );
             }
 
